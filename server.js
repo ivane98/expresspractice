990425be509bb59d5b905d4c8c9271b1e0c31357
ivane98 <i.vardoshvili22@gmail.com>
2023-03-07 16:10:12 +0400

    finished

--- server.js
+++ server.js
@@ -1,21 +1,50 @@
-const path = require("path");
+require("dotenv").config();
 const express = require("express");
-const { logEvents, logger } = require("./middleware/logEvents");
+const app = express();
+const path = require("path");
 const cors = require("cors");
 const corsOptions = require("./config/corsOptions");
+const { logger } = require("./middleware/logEvents");
 const errorHandler = require("./middleware/errorHandler");
-const app = express();
+const verifyJWT = require("./middleware/verifyJWT");
+const cookieParser = require("cookie-parser");
+const credentials = require("./middleware/credentials");
+const connectDb = require("./config/dbConn");
+const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3500;
 
+connectDb();
+
+// custom middleware logger
 app.use(logger);
 
+// Handle options credentials check - before CORS!
+// and fetch cookies credentials requirement
+app.use(credentials);
+
+//middleware for cookies
+app.use(cookieParser());
+
+// Cross Origin Resource Sharing
+app.use(cors());
+
+// built-in middleware to handle urlencoded form data
+app.use(express.urlencoded({ extended: false }));
+
+// built-in middleware for json
 app.use(express.json());
 
-app.use(cors(corsOptions));
+//serve static files
+app.use("/", express.static(path.join(__dirname, "/public")));
 
-app.use(express.static(path.join(__dirname, "/public")));
-
+// routes
 app.use("/", require("./routes/root"));
+app.use("/register", require("./routes/register"));
+app.use("/auth", require("./routes/auth"));
+app.use("/refresh", require("./routes/refresh"));
+app.use("/logout", require("./routes/logout"));
+
+app.use(verifyJWT);
 app.use("/employees", require("./routes/api/employees"));
 
 app.all("*", (req, res) => {
@@ -23,12 +52,15 @@ app.all("*", (req, res) => {
   if (req.accepts("html")) {
     res.sendFile(path.join(__dirname, "views", "404.html"));
   } else if (req.accepts("json")) {
-    res.json({ error: "404 not found" });
+    res.json({ error: "404 Not Found" });
   } else {
-    res.type("text").send("404 not found");
+    res.type("txt").send("404 Not Found");
   }
 });
 
 app.use(errorHandler);
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+mongoose.connection.once("open", () => {
+  console.log("connected to mongodb");
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+});
